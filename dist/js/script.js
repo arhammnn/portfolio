1cc147fade3d1ca85efa002cb26ba40d4102a9c4
arham <muh.arham999@gmail.com>
2023-08-31 19:27:44 +0700

    mengubah kata

--- dist/js/script.js
+++ dist/js/script.js
@@ -55,3 +55,22 @@ if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.match
   } else {
     darkToggle.checked = false;
   }
+
+// alert
+
+window.addEventListener("load", function() {
+    const form = document.getElementById('my-form');
+    form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: 'POST',
+      body: data,
+    })
+    .then(() => {
+      alert("Pesan berhasil terkirim!");
+    })
+  });
+});
+  
