e75bf6db51a55f366f99b5cb284554c2523804ea
arhammnn <105142902+arhammnn@users.noreply.github.com>
2023-09-01 09:52:18 +0700

    Update script.js

--- dist/js/script.js
+++ dist/js/script.js
@@ -55,3 +55,19 @@ if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.match
   } else {
     darkToggle.checked = false;
   }
+
+    window.addEventListener("load", function() {
+    const form = document.getElementById('my-form');
+    form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: 'POST',
+      body: data,
+    })
+    .then(() => {
+      alert("Pesan berhasil terkirim!");
+    })
+  });
+});
