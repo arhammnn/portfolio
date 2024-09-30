518358cd6dcf855c7cab57afd8b56e037b40c8df
arham <muh.arham999@gmail.com>
2024-09-30 17:23:07 +0800

    update link

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,83 +1,72 @@
 // Navbar-fixed
 window.onscroll = function() {
-    const header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
-    const toTop = document.querySelector('#to-top');
+  const header = document.querySelector('header');
+  const fixedNav = header.offsetTop;
+  const toTop = document.querySelector('#to-top');
 
-    if(window.pageYOffset > fixedNav) {
-    header.classList.add('navbar-fixed');
-    toTop.classList.remove('hidden');
-    toTop.classList.add('flex');
-} else {
-    header.classList.remove('navbar-fixed');
-        toTop.classList.remove('flex');
-        toTop.classList.add('hidden');
-    }
+  if (window.pageYOffset > fixedNav) {
+      header.classList.add('navbar-fixed');
+      toTop.classList.remove('hidden');
+      toTop.classList.add('flex');
+  } else {
+      header.classList.remove('navbar-fixed');
+      toTop.classList.remove('flex');
+      toTop.classList.add('hidden');
+  }
 };
-
-
 
 // Hamburger
 const hamburger = document.querySelector('#hamburger');
 const navMenu = document.querySelector('#nav-menu'); 
 
 hamburger.addEventListener('click', function () {
-    hamburger.classList.toggle('hamburger-active');
-    navMenu.classList.toggle('hidden');
+  hamburger.classList.toggle('hamburger-active');
+  navMenu.classList.toggle('hidden');
 });
 
 // Klik di luar hamburger
 window.addEventListener('click', function (e) {
-    if(e.target != hamburger && e.targer != navMenu) {
-        hamburger.classList.remove('hamburger-active');
-        navMenu.classList.add('hidden');
-    }
+  if (e.target != hamburger && e.target != navMenu) {
+      hamburger.classList.remove('hamburger-active');
+      navMenu.classList.add('hidden');
+  }
 });
 
-//Darkmode toggle
+// Darkmode toggle
 const darkToggle = document.querySelector('#dark-toggle');
 const html = document.querySelector('html');
 
 darkToggle.addEventListener('click', function () {
-    if (darkToggle.checked) {
-        html.classList.add('dark');
-        localStorage.theme = 'dark';
-    } else {
-        html.classList.remove('dark');
-        localStorage.theme = 'light';
-    }
+  if (darkToggle.checked) {
+      html.classList.add('dark');
+      localStorage.theme = 'dark';
+  } else {
+      html.classList.remove('dark');
+      localStorage.theme = 'light';
+  }
 });
 
-// pidahkan posisi toggle sesuai mode
+// Pindahkan posisi toggle sesuai mode
 // On page load or when changing themes, best to add inline in `head` to avoid FOUC
 if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    darkToggle.checked = true;
-  } else {
-    darkToggle.checked = false;
-  }
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
 
-<<<<<<< HEAD
-// alert
-
+// Alert
 window.addEventListener("load", function() {
-=======
-    window.addEventListener("load", function() {
->>>>>>> ad6f7855539c54f61877efbde13e13ac9a51a5b7
-    const form = document.getElementById('my-form');
-    form.addEventListener("submit", function(e) {
-    e.preventDefault();
-    const data = new FormData(form);
-    const action = e.target.action;
-    fetch(action, {
-      method: 'POST',
-      body: data,
-    })
-    .then(() => {
-      alert("Pesan berhasil terkirim!");
-    })
+  const form = document.getElementById('my-form');
+  form.addEventListener("submit", function(e) {
+      e.preventDefault();
+      const data = new FormData(form);
+      const action = e.target.action;
+      fetch(action, {
+          method: 'POST',
+          body: data,
+      })
+      .then(() => {
+          alert("Pesan berhasil terkirim!");
+      });
   });
 });
-<<<<<<< HEAD
-  
-=======
->>>>>>> ad6f7855539c54f61877efbde13e13ac9a51a5b7
